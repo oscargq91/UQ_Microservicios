81fe4b67b47bbf17805444abbdbda107cb012f6f
oegarces <oegarces@bancolombia.com.co>
2024-05-01 07:13:22 -0500

    update

---
+++ taler_logs/applications/app-service/src/main/java/co/com/bancolombia/config/ReadinessStateHealthIndicator.java
@@ -0,0 +1,29 @@
+package co.com.bancolombia.config;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.lang.management.ManagementFactory;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ReadinessStateHealthIndicator implements ReactiveHealthIndicator {
+    private static final LocalDateTime START_TIME = LocalDateTime.now();
+
+    @Override
+    public Mono<Health> health() {
+
+        return Mono.just(Health.up()
+                .withDetail("from", START_TIME)
+                .withDetail("status", "ALIVE")
+                .withDetail("uptimeSeconds", getUptimeSeconds())
+                .build());
+    }
+    private long getUptimeSeconds() {
+        long uptimeMillis = ManagementFactory.getRuntimeMXBean().getUptime();
+        return Duration.ofMillis(uptimeMillis).getSeconds();
+    }
+}
